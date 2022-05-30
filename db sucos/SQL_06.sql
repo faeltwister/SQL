a17ac8d527ada8e45380e41101dc322f6ff7cc66
Rafael Oliveira <faeltwister@gmail.com>
2022-05-30 15:06:03 -0300

    update valores
    alterando os valores do campo preço na tabela tb_produto

---
+++ db sucos/SQL_06.sql
@@ -0,0 +1,17 @@
+SELECT * FROM sucos.tb_produto;
+
+UPDATE tb_produto set preco = 4.90 where SKU = '1042712';
+UPDATE tb_produto set preco = 18.01 where SKU = '788975';
+UPDATE tb_produto set preco = 28.51 where SKU = '695594';
+UPDATE tb_produto set preco = 3.76 where SKU = '479745';
+UPDATE tb_produto set preco = 16.51 where SKU = '326779';
+UPDATE tb_produto set preco = 4.21 where SKU = '229900';
+UPDATE tb_produto set preco = 11.01 where SKU = '1086543';
+UPDATE tb_produto set preco = 5.17 where SKU = '1078680';
+UPDATE tb_produto set preco = 3.29 where SKU = '1051518';
+UPDATE tb_produto set preco = 13.29 where SKU = '231776';
+UPDATE tb_produto set preco = 9.01 where SKU = '1101035';
+UPDATE tb_produto set preco = 2.45 where SKU = '544931';
+UPDATE tb_produto set preco = 2.45 where SKU = '838819';
+
+
