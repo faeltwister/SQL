a773fea64a0c88e1782fefa0303e7acb2295ee80
Rafael Oliveira <faeltwister@gmail.com>
2022-05-30 14:53:48 -0300

    Add files via upload
    ADD mais dados nas tabelmas

---
+++ db sucos/SQL_05.sql
@@ -0,0 +1,16 @@
+USE SUCOS;
+
+INSERT INTO tb_produto (SKU, DESCRICAO, EMBALAGEM, PRECO) VALUES ('544931','Frescor do Verão - 350 ml - Limão','Lata',2.4595);
+INSERT INTO tb_produto (SKU, DESCRICAO, EMBALAGEM, PRECO) VALUES ('1078680','Frescor do Verão - 470 ml - Manga','Garrafa',5.1795);
+INSERT INTO tb_produto (SKU, DESCRICAO, EMBALAGEM, PRECO) VALUES ('1042712','Linha Citros - 700 ml - Limão','Garrafa',4.904);
+INSERT INTO tb_produto (SKU, DESCRICAO, EMBALAGEM, PRECO) VALUES ('788975','Pedaços de Frutas - 1,5 Litros - Maça','PET',18.011);
+INSERT INTO tb_produto (SKU, DESCRICAO, EMBALAGEM, PRECO) VALUES ('1002767','Videira do Campo - 700 ml - Cereja/Maça','Garrafa',8.41);
+INSERT INTO tb_produto (SKU, DESCRICAO, EMBALAGEM, PRECO) VALUES ('231776','Festival de Sabores - 700 ml - Açai','Garrafa',13.312);
+INSERT INTO tb_produto (SKU, DESCRICAO, EMBALAGEM, PRECO) VALUES ('479745','Clean - 470 ml - Laranja','Garrafa',3.768);
+INSERT INTO tb_produto (SKU, DESCRICAO, EMBALAGEM, PRECO) VALUES ('1051518','Frescor do Verão - 470 ml - Limão','Garrafa',3.2995);
+INSERT INTO tb_produto (SKU, DESCRICAO, EMBALAGEM, PRECO) VALUES ('1101035','Linha Refrescante - 1 Litro - Morango/Limão','PET',9.0105);
+INSERT INTO tb_produto (SKU, DESCRICAO, EMBALAGEM, PRECO) VALUES ('229900','Pedaços de Frutas - 350 ml - Maça','Lata',4.211);
+INSERT INTO tb_produto (SKU, DESCRICAO, EMBALAGEM, PRECO) VALUES ('1086543','Linha Refrescante - 1 Litro - Manga','PET',11.0105);
+INSERT INTO tb_produto (SKU, DESCRICAO, EMBALAGEM, PRECO) VALUES ('695594','Festival de Sabores - 1,5 Litros - Açai','PET',28.512);
+INSERT INTO tb_produto (SKU, DESCRICAO, EMBALAGEM, PRECO) VALUES ('838819','Clean - 1,5 Litros - Laranja','PET',12.008);
+INSERT INTO tb_produto (SKU, DESCRICAO, EMBALAGEM, PRECO) VALUES ('326779','Linha Refrescante - 1,5 Litros - Manga','PET',16.5105);
