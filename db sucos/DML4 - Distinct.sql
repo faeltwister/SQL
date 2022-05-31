d6e4ba77be9b87f13851b86798b3c6a75242242e
Rafael Oliveira <faeltwister@gmail.com>
2022-05-31 10:16:17 -0300

    DML - Distinct
    Limita a consulta buscando os primeiros termos ou mais

---
+++ db sucos/DML4 - Distinct.sql
@@ -0,0 +1,10 @@
+select * from tabela_de_produtos;
+
+#seleciona os 5 primeiros
+select * from tabela_de_produtos Limit 5;
+
+#seleciona na posição 2 até o 3, tirando o primeiro da tabela
+select * from tabela_de_produtos Limit 2,3;
+
+#seleciona o primeiro e os dois posteriores
+select * from tabela_de_produtos Limit 0,3;
